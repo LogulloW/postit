ec42897b6c591f55d69cfe6a2f68c94de197df56
William Logullo <williamlogullo@gmail.com>
2022-07-16 08:59:02 -0300

    postit

---
+++ test.ts
@@ -0,0 +1,22 @@
+class Student {
+    constructor(name: string, age: number, isGraduated: boolean){
+        this.name = name;
+        this.age = age;
+        this.isGraduated = isGraduated;
+    }
+
+    public name: string = '';
+    public age: number = 0;
+    public isGraduated: boolean = false;
+
+    public studentFormattedData(): string {
+        return 'Nome: ' + this.name + 'Idade: ' + this.age;
+    }
+}
+
+const student = new Student('Henrique', 24, true);
+
+if (student.isGraduated) {
+    const dataFormated = student.studentFormattedData()
+    console.log(dataFormated);
+}
