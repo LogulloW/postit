31d6a9e92fbc59177c89b711fa9a280980ad3c2f
William Logullo <williamlogullo@gmail.com>
2022-07-20 08:26:35 -0300

    update(navbar) Configurado icones

--- postit/src/app/components/navbar/navbar.component.ts
+++ postit/src/app/components/navbar/navbar.component.ts
@@ -1,14 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs/operators';
+import { NavbarEnum } from 'src/app/models/enums/navbar.enum';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss'],
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent {
 
-  constructor() { }
+  constructor(
+    private readonly router: Router,
+  ) { 
+    router.events
+    .pipe(filter((event) => event instanceof NavigationEnd))
+    .subscribe((route: NavigationEnd) => {
+    if (this.router.url.includes('/feed'))
+      this.currentNavbar = NavbarEnum.FEED;
+    if (this.router.url.includes('/home'))
+      this.currentNavbar = NavbarEnum.HOME;
+    if (this.router.url.includes('/profile'))
+      this.currentNavbar = NavbarEnum.PROFILE;
+      });
+  }
 
-  ngOnInit() {}
-
+  public navbarEnum: typeof NavbarEnum = NavbarEnum;
+  public currentNavbar: NavbarEnum = NavbarEnum.HOME;
 }
